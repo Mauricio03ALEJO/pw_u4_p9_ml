4a3a670f8dd8ae51eb97b3707fe05dfba73ab92f
mauro <mauroalejo2000@hotmail.com>
2026-02-05 18:43:38 -0500

    Taller36

--- src/router/index.js
+++ src/router/index.js
@@ -9,44 +9,88 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+      meta: {
+        requireAutorizacion: true,
+        esPublica: false
+      }
     },
     {
       path: '/about',
       name: 'about',
       component: () => import('../views/AboutView.vue'),
+      meta: {
+        requireAutorizacion: false,
+        esPublica: true
+      }
     },
     // Rutas para estudiantes (sin /estudiantes)
     {
       path: '/consultarTodos',
       name: 'consultarTodos',
       component: () => import('../views/ConsultarTodos.vue'),
+      meta: {
+        requireAutorizacion: true,
+        esPublica: false
+      }
     },
     {
       path: '/consultarPorID',
       name: 'consultarPorID',
       component: () => import('../views/ConsultarPorID.vue'),
+      meta: {
+        requireAutorizacion: false,
+        esPublica: true
+      }
     },
     {
       path: '/guardar',
       name: 'guardarEstudiante',
       component: () => import('../views/GuardarEstudiante.vue'),
+      meta: {
+        requireAutorizacion: true,
+        esPublica: false
+      }
     },
     {
       path: '/actualizar',
       name: 'actualizarEstudiante',
       component: () => import('../views/ActualizarEstudiante.vue'),
+      meta: {
+        requireAutorizacion: false,
+        esPublica: true
+      }
     },
     {
       path: '/actualizarParcial',
       name: 'actualizarParcialEstudiante',
       component: () => import('../views/ActualizarParcialEstudiante.vue'),
+      meta: {
+        requireAutorizacion: true,
+        esPublica: false
+      }
     },
     {
       path: '/eliminar',
       name: 'eliminarEstudiante',
       component: () => import('../views/EliminarEstudiante.vue'),
+      meta: {
+        requireAutorizacion: false,
+        esPublica: true
+      }
     },
   ],
+})
+
+/* Configuracion del Guardian */
+router.beforeEach((to, from, next) => {
+  if (to.meta.requireAutorizacion){
+    /* le envio a una pagina de login */
+    console.log("Redirigiendo a login");
+  }else {
+    /* le dejo sin validaciones */
+    console.log("Pase libre");
+    next();
+  }
 })
 
 export default router
